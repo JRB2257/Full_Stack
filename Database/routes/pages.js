fb48a4250f9bec257caaffd8b5d2bd32e479eea4
JRB2257 <59499483+JRB2257@users.noreply.github.com>
2022-11-27 14:01:16 +0530

    Add files via upload

---
+++ Database/routes/pages.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const loggedIn = require("../controllers/loggedIn")
+const router = express.Router();
+
+router.get("/", loggedIn, (req, res) => {
+    if (req.user) {
+        res.render("index", { status: "loggedIn" , user: req.user });
+    } else {
+        res.render("index", { status: "no", user: "nothing" });
+    }
+})
+router.get("/register",(req, res) => {
+    res.sendFile("register.html", { root: "./public"});
+})
+router.get("/login",(req, res) => {
+    res.sendFile("login.html", { root: "./public"});
+})
+router.get("/logout",(req, res) => {
+    res.sendFile("logout.html", { root: "./public"});
+})
+module.exports = router;
